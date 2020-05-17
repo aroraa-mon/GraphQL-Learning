598330d9e536b00e34b466e060466f255480a0e2
Abhishek Arora <abhishek.arora@monotype.com>
2020-05-17 16:12:41 +0530

    moving resolver to separate file

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
-
+import resolver from './resolver';
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('Grapgql is amazing');
 })
 
-class Friend {
-    constructor(id, {firstname, lastname, gender, language}){
-        this.id = id;
-        this.firstname = firstname,
-        this.lastname = lastname,
-        this.gender = gender,
-        this.language = language
-    }
-}
-
-const FriendDb = {};
-
-const root = { 
-    friend: () => {
-        return {
-            "id": 114,
-            "firstname": "Rahul",
-            "lastname": "Gupta",
-            "gender": "Male",
-            "language": "Hindi",
-            "emails": "abc.com"
-        }
-    },
-    createFriend: ({input})  =>  {
-        let id = "a1b2b3";
-        FriendDb[id] = input;
-        return new Friend(id, input);
-    }
-
-};
+const root = resolver;
 
 app.use('/graphql', graphqlHTTP({
     schema,
---
+++ resolver.js
@@ -0,0 +1,38 @@
+class Friend {
+    constructor(id, {firstname, lastname, gender, language}){
+        this.id = id;
+        this.firstname = firstname,
+        this.lastname = lastname,
+        this.gender = gender,
+        this.language = language
+    }
+}
+
+const FriendDb = {};
+
+const resolver = { 
+    /* friend: () => {
+        return {
+            "id": 114,
+            "firstname": "Rahul",
+            "lastname": "Gupta",
+            "gender": "Male",
+            "language": "Hindi",
+            "emails": "abc.com"
+        }
+    }, */
+    getFriend: ({id}) => {
+        console.log(id);
+        console.log(FriendDb);
+        return new Friend(id,FriendDb[id]);
+    },
+    createFriend: ({input})  =>  {
+        let id = "a1b2b3";
+        FriendDb[id] = input;
+        return new Friend(id, input);
+    }
+
+};
+
+export default resolver;
+
